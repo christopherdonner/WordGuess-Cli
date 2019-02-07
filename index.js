17bbe3f347eb52d5683d117bef8cfb0952ef5870
Christopher Donner <christopher.donner@gmail.com>
2019-02-07 14:47:46 -0500

    some stuff working

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ var inquirer = require("inquirer");
 var letterInclude = require("./letter.js")
 var wordInclude = require ("./word.js")
 
+function prompt(){
+inquirer.prompt([
+
+    {
+      type: "input",
+      name: "guess",
+      message: "Guess a letter"
+    }]).then(function(){prompt();})
+
+}
+
+wordInclude.pickWord();
+console.log(wordInclude.word)
+prompt();
--- word.js
+++ word.js
@@ -1,5 +1,19 @@
 var letterInclude = require("./letter.js")
 
+var secretWord=[]
+var word0=["m","a","g","g","i","e"]
+var word1=["m","a","r","g","e"]
+var word2=["b","a","r","t"]
+var word3=["h","o","m","e","r"]
+var word4=["l","i","s","a"]
+var word5=["m","i","l","l","h","o","u","s","e"]
+var word6=["s","p","r","i","n","g","f","i","e","l","d"]
+var word7=["f","l","a","n","d","e","r","s"]
+var word8=["k","r","u","s","t","y"]
+var word9=["w","i","g","g","u","m"]
+var wordsList=[word0, word1, word2, word3, word4,word5,word6, word7, word8, word9]
+// var currentWord=""
+
 function word (New)
 {
     this.New=New
@@ -7,4 +21,21 @@ function word (New)
 
 var currentLetter=new letterInclude.letter("a", false)
 
-console.log(currentLetter.value)
+function pickWord()
+{
+    var currentWord=""
+    //pick random word from wordsList array
+    wordPickerInt=Math.floor(Math.random() * wordsList.length);
+    currentWord=wordsList[wordPickerInt]; //set as currentWord
+    console.log(currentWord)
+    for(i=0;i<currentWord.length;i++)
+        {}
+    }
+
+// console.log(currentLetter.value)
+currentLetter.show()
+
+module.exports={
+    word: word,
+    pickWord: pickWord
+}
