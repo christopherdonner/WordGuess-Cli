99bc2b964fb633366f7ab1b123aea6704ef3c018
christopherdonner <christopher.donner@gmail.com>
2019-02-09 22:00:52 -0500

    objects being created correctly. check condition not firing correctly.

--- index.js
+++ index.js
@@ -1,19 +1,75 @@
 var inquirer = require("inquirer");
 
 var letterInclude = require("./letter.js")
-var wordInclude = require ("./word.js")
+var wordInclude = require("./word.js")
 
-function prompt(){
-inquirer.prompt([
+
+var secretWord = []
+var word0 = ["m", "a", "g", "g", "i", "e"]
+var word1 = ["m", "a", "r", "g", "e"]
+var word2 = ["b", "a", "r", "t"]
+var word3 = ["h", "o", "m", "e", "r"]
+var word4 = ["l", "i", "s", "a"]
+var word5 = ["m", "i", "l", "l", "h", "o", "u", "s", "e"]
+var word6 = ["s", "p", "r", "i", "n", "g", "f", "i", "e", "l", "d"]
+var word7 = ["f", "l", "a", "n", "d", "e", "r", "s"]
+var word8 = ["k", "r", "u", "s", "t", "y"]
+var word9 = ["w", "i", "g", "g", "u", "m"]
+var wordsList = [word0, word1, word2, word3, word4, word5, word6, word7, word8, word9]
+// var currentWord=""
+var currentWord = ""
+var wordArray = []
+var secretWord=""
+
+function pickWord() {
+  wordPickerInt = Math.floor(Math.random() * wordsList.length);
+  currentWord = wordsList[wordPickerInt]; //set as currentWord
+  console.log(currentWord)
+}
+
+if(currentWord){
+  secretWord = new wordInclude.word(currentWord)
+}
+console.log(secretWord)
+function prompt() {
+  inquirer.prompt([
 
     {
       type: "input",
       name: "guess",
       message: "Guess a letter"
-    }]).then(function(){prompt();})
-
+    }]).then(function (input) {
+      console.log(input.guess)
+      for(i=0;i<secretWord.length;i++){
+        console.log(wordInclude.wordArray[i]+"test")
+        if(input.guess===wordInclude.wordArray[i]){
+          wordInclude.wordArray[i].guessed=true
+        }
+      }
+    currentWord.makeWord();  
+    prompt();
+  })
+  
 }
 
-wordInclude.pickWord();
+// wordInclude.pickWord();
+
+
+console.log(currentWord)
+//   // wordInclude.pickWord();
+//   function pickWord()
+// {
+//     //pick random word from wordsList array
+//     var wordPickerInt=Math.floor(Math.random() * wordsList.length);
+//     wordInclude.currentWord=wordsList[wordPickerInt]; //set as currentWord
+//     console.log(currentWord)
+    
+
+// }
+
+
+pickWord();
 console.log(wordInclude.word)
-prompt();
+  var currentWord= new wordInclude.word(currentWord)
+  currentWord.makeWord();
+  prompt();
--- word.js
+++ word.js
@@ -1,41 +1,24 @@
 var letterInclude = require("./letter.js")
 
-var secretWord=[]
-var word0=["m","a","g","g","i","e"]
-var word1=["m","a","r","g","e"]
-var word2=["b","a","r","t"]
-var word3=["h","o","m","e","r"]
-var word4=["l","i","s","a"]
-var word5=["m","i","l","l","h","o","u","s","e"]
-var word6=["s","p","r","i","n","g","f","i","e","l","d"]
-var word7=["f","l","a","n","d","e","r","s"]
-var word8=["k","r","u","s","t","y"]
-var word9=["w","i","g","g","u","m"]
-var wordsList=[word0, word1, word2, word3, word4,word5,word6, word7, word8, word9]
-// var currentWord=""
 
-function word (New)
-{
-    this.New=New
+function word(value) {
+    this.value = value,
+    // this.wordArray=wordArray
+    this.makeWord = function () {
+        for (i = 0; i < value.length; i++) {
+            var currentLetter = new letterInclude.letter(value[i], false)
+            var wordArray=[]
+            currentLetter.show()
+            // console.log(currentLetter)
+            wordArray.push(currentLetter)
+            // console.log(value[i])
+        }
+    }
 }
 
-var currentLetter=new letterInclude.letter("a", false)
+var tempWord=new word("test")
+// console.log(tempWord)
 
-function pickWord()
-{
-    var currentWord=""
-    //pick random word from wordsList array
-    wordPickerInt=Math.floor(Math.random() * wordsList.length);
-    currentWord=wordsList[wordPickerInt]; //set as currentWord
-    console.log(currentWord)
-    for(i=0;i<currentWord.length;i++)
-        {}
-    }
-
-// console.log(currentLetter.value)
-currentLetter.show()
-
-module.exports={
-    word: word,
-    pickWord: pickWord
+module.exports = {
+    word: word
 }
